f80c4c8f94e214c78b6dea835c8b12d251785f68
devolo13 <devolo13@gmail.com>
2023-04-06 20:32:10 -0500

    initial commit

---
+++ server.py
@@ -0,0 +1,30 @@
+import random
+from flask import Flask, render_template, request, redirect, session
+app = Flask(__name__)
+app.secret_key = 'Harry Potter was mid'
+
+
+@app.route('/')
+def landing_page():
+    return render_template('index.html')
+
+
+@app.route('/submit', methods=["GET", "POST"])
+def process_submission():
+    print(request.form)
+    session['name'] = request.form['name']
+    session['dojo_location'] = request.form['dojo_location']
+    session['favorite_language'] = request.form['favorite_language']
+    session['comment'] =  request.form['comment']
+    return render_template('results.html')
+
+
+@app.route('/destroy_session')
+def reset_session():
+    # RESET SESSION COOKIES AND REDIRECT TO '/'
+    session.clear()
+    return redirect('/')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
